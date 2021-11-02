7b5eaf67d15d99edc1008e9fcecb3716d2492ab6
davidwah <davidwahyuyu@gmail.com>
2021-11-02 18:01:43 +0700

    belajar variabel

---
+++ variable/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func main() {
+	var name string
+
+	name = "Wahyu"
+	fmt.Println(name)
+
+	var friendName = "Joko"
+	fmt.Println(friendName)
+
+	age := 30
+	fmt.Println(age)
+
+	var (
+		firstName = "David"
+		lastName  = "Wahyu"
+	)
+	fmt.Println(firstName)
+	fmt.Println(lastName)
+}
