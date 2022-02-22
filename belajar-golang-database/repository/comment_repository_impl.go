574dced40445375c0a32251e705e7344d0012ee2
David Wahyu Pratomo <22112331+davidwah@users.noreply.github.com>
2022-02-23 02:04:38 +0700

    add repository

---
+++ belajar-golang-database/repository/comment_repository_impl.go
@@ -0,0 +1,69 @@
+package repository
+
+import (
+	"belajar-golang-database/entity"
+	"context"
+	"database/sql"
+	"errors"
+	"strconv"
+)
+
+type commentRepositoryImpl struct {
+	DB *sql.DB
+}
+
+func NewCommentRepository(db *sql.DB) CommentRepository {
+	return &commentRepositoryImpl{DB: db}
+
+}
+
+func (repository *commentRepositoryImpl) Insert(ctx context.Context, comment entity.Comment) (entity.Comment, error) {
+	//TODO implement me
+	script := "insert into comments(email, comment) values (?, ?)"
+	result, err := repository.DB.ExecContext(ctx, script, comment.Email, comment.Comment)
+	if err != nil {
+		return comment, err
+	}
+	id, err := result.LastInsertId()
+	if err != nil {
+		return comment, err
+	}
+	comment.Id = int32(id)
+	return comment, nil
+}
+
+func (repository *commentRepositoryImpl) FindById(ctx context.Context, id int32) (entity.Comment, error) {
+	//TODO implement me
+	script := "select id, email, comment from comments where id = ? limit 1"
+	rows, err := repository.DB.QueryContext(ctx, script, id)
+	comment := entity.Comment{}
+	if err != nil {
+		return comment, err
+	}
+	defer rows.Close()
+	if rows.Next() {
+		//	ada
+		rows.Scan(&comment.Id, &comment.Email, &comment.Comment)
+		return comment, nil
+	} else {
+		//	tidak ada
+		return comment, errors.New("Id " + strconv.Itoa(int(id)) + " Not Found")
+	}
+}
+
+func (repository *commentRepositoryImpl) FindAll(ctx context.Context) ([]entity.Comment, error) {
+	//TODO implement me
+	script := "select id, email, comment from comments"
+	rows, err := repository.DB.QueryContext(ctx, script)
+	if err != nil {
+		return nil, err
+	}
+	defer rows.Close()
+	var comments []entity.Comment
+	for rows.Next() {
+		comment := entity.Comment{}
+		rows.Scan(&comment.Id, &comment.Email, &comment.Comment)
+		comments = append(comments, comment)
+	}
+	return comments, nil
+}
